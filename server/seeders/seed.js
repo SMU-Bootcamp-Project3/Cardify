4946a564f6240d0259bec2c8f48aae891d1d9336
valerierobles <roblesv08@gmail.com>
2023-05-05 20:08:35 -0500

    seed files outlines

---
+++ server/seeders/seed.js
@@ -0,0 +1,4 @@
+const db = require('../config/connection');
+const { User, Card } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const cardSeeds = require('./cardSeeds.json');
