2a16d921b1fb27f6a75808110184da72cc37a0d0
Giuliana C <117243702+gj3chavez@users.noreply.github.com>
2023-05-13 12:54:29 -0500

    writes back Auth.getProfile
    Signed-off-by: Giuliana C. <117243702+gj3chavez@users.noreply.github.com>

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -23,6 +23,7 @@ const Header = () => {
           {Auth.loggedIn() ? (
             <>
               <Link className="btn btn-lg btn-primary m-2" to="/me">
+            {Auth.getProfile().data.username}
                 View My Profile
               </Link>
               <button className="btn btn-lg btn-light m-2" onClick={logout}>
