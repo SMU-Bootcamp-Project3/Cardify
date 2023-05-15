4c607314c1ee821459126facae166cd1d33f71fe
gj3chavez <117243702+gj3chavez@users.noreply.github.com>
2023-05-15 16:56:02 -0500

    deploying

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,89 +1,62 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { useState, useEffect } from 'react'
-import {Button} from "react-bootstrap"
 
-import Card from '../components/Card/Card'
-import Form from '../components/Form/Form';
-import FormModal from '../components/FormModal/FormModal';
+import React from "react";
+// import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../products";
+import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
+import { QUERY_CARDS } from "../../utils/queries";
+import { useQuery } from "@apollo/client";
+// import { Link } from "react-router-dom";
 
-import { QUERY_CARDS }  from '../utils/queries'; 
+import "./cart.css";
+export const Home = () => {
+  const { cartItems, getTotalCartAmount, checkout } = useQuery(QUERY_CARDS);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cart">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={
+                {
+                    ...product,
+                    amount: cartItems[product.id],
+                }
 
 
-const Home = () => {
-    // const { loading, data } = useQuery(QUERY_CARDS);
-    // const cards = data?.cards || [];
-    const [ showModal, setShowModal ] = useState(false)
-  const [ cardDetails, setCardDetails ] = useState({
-    holiday: '',
-    senderName: '',
-    recipientName: '',
-    customMessage: '',
-    message: 'Happy Holidays',
-    messageType: '',
-    imageUpload: ''
-  })
+            } />;
+          }
+        })}
+      </div>
 
-  const handleUpload = (event) => {
-    console.log(event.target.files[0].name)
-    setCardDetails({
-      ...cardDetails, 
-      imageUpload: URL.createObjectURL(event.target.files[0])
-    })
-}
-
-  const handleInputChange = (event) => {
-    setCardDetails({
-      ...cardDetails,
-      [event.target.name]: event.target.value
-    })
-  }
-
-  const handleShow = () => {
-    setShowModal(true)
-  }
-
-  useEffect(() => {
-    // setShowModal(true)
-  }, [])
-
-  const closeModal = () => {
-    setShowModal(false)
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    setShowModal(false)
-    // Submit form data and redirect to page with the greeting card component displayed
-    setCardDetails({
-      ...cardDetails,
-      [event.target.name]: event.target.value
-    })
-
-    console.log(cardDetails)  
-  }
-
-    return (
-        <main>
-              <h1 style={{margin: '2rem 0', fontFamily: `'Abel', sans-serif`}}>Click to create your own greeting card</h1>
-              <Button variant="primary" onClick={handleShow}>
-                Create your custom greeting card!
-              </Button>
-                <Card cardInfo={cardDetails} />
-              <FormModal
-                show={showModal}
-                handleClose={closeModal}
-                handleSubmit={handleSubmit}
-              >
-                <Form 
-                handleSubmit={handleSubmit} 
-                cardDetails={cardDetails} handleInputChange={handleInputChange}
-                handleUpload={handleUpload}
-                />
-              </FormModal>
-            </main>
-    );
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p> Subtotal: ${totalAmount} </p>
+          <button onClick={() => navigate("/")}>Perzonalize Card</button>
+          <button
+            onClick={() => {
+              checkout();
+              navigate("/stripe");
+            }}
+          >
+            {" "}
+            Checkout{" "}
+          </button>
+        </div>
+      ) : (
+        <h1> Your Shopping Cart is Empty</h1>
+      )}
+      <>
+        
+      </>
+    </div>
+  );
 };
 
-export default Home; 
+export default Home;
